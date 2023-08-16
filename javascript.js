c43d141c6b51ff1cc9722b8a7adc165ec8d00c35
0BBLER <125929577+0BBLER@users.noreply.github.com>
2023-08-16 06:33:23 -0400

    Added main structure

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function c1() {
+}
+function c2() {
+    open("https://github.com/0BBLER");
+}
+function c3() {
+    open("https://www.google.com/search?q=pig");
+}
+function c4() {
+    open("https://www.minecraft.net/");
+}
+function c5() {
+    open("https://www.java.com/");
+}
+function c6() {
+}
+function c7() {
+}
+function c8() {
+}
+function c9() {
+}
+function c10() {
+}
+function c11() {
+}
